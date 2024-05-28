98dfe7c154664ba01cc9dab850159abc02f4142b
Faridrhmn <bfaridrahman@gmail.com>
2024-05-28 18:42:43 +0700

    update model

--- model.js
+++ model.js
@@ -1,4 +1,5 @@
 var dbConn = require("./db");
+const bcrypt = require('bcrypt');
 
 var Account = function(user){
     this.username = user.username;
@@ -20,20 +21,48 @@ Account.create = function(newAccount, result){
     }
 }
 
+// Account.findUser = function(newAccount, result) {
+//     const query = "SELECT * FROM akun WHERE username = ? AND password = ?";
+//     dbConn.query(query, [
+//         newAccount.username, 
+//         newAccount.password], function(err, res) {
+//         if (err) {
+//         console.log("Error: ", err);
+//         result(err, null);
+//         } else {
+//             if (res.length > 0) {
+//                 console.log("User found: ", res);
+//                 result(null, res);
+//             } else {
+//                 console.log("No user found with the given username and password.");
+//                 result(null, []);
+//             }
+//         }
+//     });
+// };
+
 Account.findUser = function(newAccount, result) {
-    const query = "SELECT * FROM akun WHERE username = ? AND password = ?";
-    dbConn.query(query, [
-        newAccount.username, 
-        newAccount.password], function(err, res) {
+    const query = "SELECT * FROM akun WHERE username = ?";
+    dbConn.query(query, [newAccount.username], function(err, res) {
         if (err) {
-        console.log("Error: ", err);
-        result(err, null);
+            console.log("Error: ", err);
+            result(err, null);
         } else {
             if (res.length > 0) {
-                console.log("User found: ", res);
-                result(null, res);
+                // Compare the hashed password with the stored hashed password
+                bcrypt.compare(newAccount.password, res[0].password, function(err, isMatch) {
+                    if (err) {
+                        result(err, null);
+                    } else if (isMatch) {
+                        console.log("User found: ", res);
+                        result(null, res);
+                    } else {
+                        console.log("Incorrect password.");
+                        result(null, []);
+                    }
+                });
             } else {
-                console.log("No user found with the given username and password.");
+                console.log("No user found with the given username.");
                 result(null, []);
             }
         }
